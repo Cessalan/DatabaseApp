adbc9c5c8cc59d559c6a71097ba4e73a20b9dc38
Cessalan <31516973+Cessalan@users.noreply.github.com>
2019-10-16 20:02:51 -0400

    Add project files.

---
+++ Database/DB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class DB
+    {
+        public static DemoEntities myDb = new DemoEntities();
+
+        public static IEnumerable<Product> GetProducts()
+        {
+            return myDb.Products.ToList();
+        }
+
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            return myDb.Customers.ToList();
+        }
+
+        public static IEnumerable<Sale> GetSalesList()
+        {
+            return myDb.Sales.ToList();
+        }
+       
+
+        public static String updateCustomer(int id)
+        {
+            String fname = "NEW";
+            String lname = "CUS";
+            Customer cus = myDb.Customers.Find(id);
+            cus.CustomerFirstName = fname;
+            cus.CustomerLastName = lname;
+            return ("Customer: " + id + " has been updated" + " First name: " + cus.CustomerFirstName + " Last name:" + cus.CustomerLastName);
+
+        }
+
+        public static String deleteProduct(int id)
+        {
+            Product p = myDb.Products.Find(id);
+            myDb.Products.Remove(p);
+            myDb.SaveChanges();
+
+            return ("Product with id: "+id+" has been deleted");
+        }
+    }
+}
---
+++ DatabaseApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace DatabaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+
+
+            // display Sales
+            IEnumerable<Sale> sales = DB.GetSalesList();
+            foreach (Sale s in sales) {
+
+                Console.WriteLine("Sales ID:  "+s.SaleId +" Customer Name: "+ 
+                    s.Customer.CustomerFirstName +" Product Description: "+ s.Product.ProductDescription);
+            }
+
+            String newCustomer = DB.updateCustomer(2);
+            Console.WriteLine(newCustomer);
+
+            String deletedProduct = DB.deleteProduct(1);
+            Console.WriteLine(deletedProduct);
+
+        }
+    }
+}
